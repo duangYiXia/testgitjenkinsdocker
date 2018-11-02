7697cf755f62132e11bcbbbebacff42ddde88ef7
zhangjingxin <zhangjingxin@feiniubus.com>
2018-11-02 17:15:59 +0800

    SpringBootServletInitializer

--- src/main/java/com/example/testgitjenkinsdocker/TestgitjenkinsdockerApplication.java
+++ src/main/java/com/example/testgitjenkinsdocker/TestgitjenkinsdockerApplication.java
@@ -2,6 +2,8 @@ package com.example.testgitjenkinsdocker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,7 +11,12 @@ import javax.servlet.http.HttpServletResponse;
 
 @SpringBootApplication
 @RestController
-public class TestgitjenkinsdockerApplication {
+public class TestgitjenkinsdockerApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(TestgitjenkinsdockerApplication.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(TestgitjenkinsdockerApplication.class, args);
